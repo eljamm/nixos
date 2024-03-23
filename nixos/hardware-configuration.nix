0594f923ab503da91c13b60d8ffaa8f4f40a24bf
eljamm <fedi.jamoussi@protonmail.ch>
2024-03-23 06:38:51 +0100

    Configure Nvidia GPU

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -15,7 +15,8 @@ in
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usbhid" "usb_storage" "sd_mod" ];
@@ -24,19 +25,22 @@ in
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/ef268b3c-6be0-4a36-9404-7c07b2ae31df";
+    {
+      device = "/dev/disk/by-uuid/ef268b3c-6be0-4a36-9404-7c07b2ae31df";
       fsType = "btrfs";
       options = [ "subvol=@" "compress-force=zstd:2" "noatime" ];
     };
 
   fileSystems."/home/kuroko/Pop" =
-    { device = "/dev/disk/by-uuid/56d64ff5-c5d0-426b-95e0-d93fa29d6d8e";
+    {
+      device = "/dev/disk/by-uuid/56d64ff5-c5d0-426b-95e0-d93fa29d6d8e";
       fsType = "btrfs";
       options = [ "compress-force=zstd:2" "noatime" ];
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/DF23-DAE8";
+    {
+      device = "/dev/disk/by-uuid/DF23-DAE8";
       fsType = "vfat";
     };
 
@@ -50,6 +54,58 @@ in
     enable = true;
     interval = "weekly";
     fileSystems = [ "/" ];
+  };
+
+  # Enable OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+
+      amdgpuBusId = "PCI:52:0:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
