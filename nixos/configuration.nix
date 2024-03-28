c9c18f94c21c07339f88939dfca287c023ca1375
eljamm <fedi.jamoussi@protonmail.ch>
2024-03-28 21:23:19 +0100

    Use Gnome instead of Cosmic
    I enabled Cosmic for testing, but it's not ready for daily driving yet.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -73,12 +73,12 @@ in
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = false;
-  services.xserver.desktopManager.gnome.enable = false;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.displayManager.cosmic-greeter.enable = true;
-  services.desktopManager.cosmic.enable = true;
+  services.displayManager.cosmic-greeter.enable = false;
+  services.desktopManager.cosmic.enable = false;
 
   # Configure keymap in X11
   services.xserver = {
