f263a38721fa8388e69c8a2d26c4e339b16bfcb8
eljamm <fedi.jamoussi@protonmail.ch>
2024-04-08 17:05:02 +0100

    Add cachix repos for nix-community and cuda

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -74,8 +74,16 @@
             ## PopOS Cosmic DE
             {
               nix.settings = {
-                substituters = [ "https://cosmic.cachix.org/" ];
-                trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+                substituters = [
+                  "https://cosmic.cachix.org/"
+                  "https://nix-community.cachix.org"
+                  "https://cuda-maintainers.cachix.org"
+                ];
+                trusted-public-keys = [
+                  "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
+                  "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+                  "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+                ];
               };
             }
             inputs.nixos-cosmic.nixosModules.default
