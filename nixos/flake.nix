f946451a242e656470f03dd72dd8b358cc07c84e
eljamm <fedi.jamoussi@protonmail.ch>
2024-04-16 10:32:56 +0100

    Add gaming flake

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -45,6 +45,11 @@
     };
 
     catppuccin.url = "github:catppuccin/nix";
+
+    nix-gaming = {
+      url = "github:fufexan/nix-gaming";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, ... } @inputs:
@@ -88,11 +93,13 @@
                   "https://cosmic.cachix.org/"
                   "https://nix-community.cachix.org"
                   "https://cuda-maintainers.cachix.org"
+                  "https://nix-gaming.cachix.org"
                 ];
                 trusted-public-keys = [
                   "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
                   "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
                   "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+                  "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
                 ];
               };
             }
