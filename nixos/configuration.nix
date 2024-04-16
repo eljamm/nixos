79b6b3cdbc2c708d03320d96959d66479f47d11f
eljamm <fedi.jamoussi@protonmail.ch>
2024-04-16 09:06:56 +0100

    Lower latency and properly set up pipewire
    Followed the official wiki

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -119,7 +119,7 @@ in
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
+  sound.enable = false;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -127,12 +127,40 @@ in
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
     jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
+  };
+
+  # Experiment with low-latency audio setup
+  services.pipewire.extraConfig.pipewire."92-low-latency" = {
+    "context.properties" = {
+      "default.clock.rate" = 48000;
+      "default.clock.quantum" = 256;
+      "default.clock.min-quantum" = 256;
+      "default.clock.max-quantum" = 256;
+    };
+  };
+
+  services.pipewire.extraConfig.pipewire-pulse."92-low-latency" = {
+    "context.modules" = [
+      {
+        name = "libpipewire-module-protocol-pulse";
+        args = {
+          "pulse.min.req" = "256/48000";
+          "pulse.default.req" = "256/48000";
+          "pulse.max.req" = "256/48000";
+          "pulse.min.quantum" = "256/48000";
+          "pulse.max.quantum" = "256/48000";
+        };
+      }
+    ];
+    "stream.properties" = {
+      "node.latency" = "256/48000";
+      "resample.quality" = 1;
+    };
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
