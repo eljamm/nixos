40dd71232de4984c9720f179af67492de0de8cd9
eljamm <fedi.jamoussi@protonmail.ch>
2024-04-18 09:05:31 +0100

    Clean up overlays

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -38,24 +38,11 @@ in
     # fenix
     (_: super: let pkgs = fenix.inputs.nixpkgs.legacyPackages.${super.system}; in fenix.overlays.default pkgs pkgs)
 
-    # Roll back to compatible wine for yabridge
     # https://github.com/NixOS/nixpkgs/issues/300755
-    (final: prev:
-      let
-        nixpkgs-wine94 = import
-          (prev.fetchFromGitHub {
-            owner = "NixOS";
-            repo = "nixpkgs";
-            rev = "f60836eb3a850de917985029feaea7338f6fcb8a"; #wineWow64Packages.stable: 9.3 -> 9.4
-            sha256 = "Ln3mD5t96hz5MoDwa8NxHFq76B+V2BOppYf1tnwFBIc=";
-          })
-          {
-            system = "x86_64-linux";
-          };
-      in
-      {
-        inherit (nixpkgs-wine94) yabridge yabridgectl;
-      })
+    (final: prev: {
+      inherit (inputs.nixpkgs-yabridge.legacyPackages.${prev.system})
+        yabridge;
+    })
   ];
 
   # Bootloader.
