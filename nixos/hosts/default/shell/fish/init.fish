435a41d3dabb569760cef49e5b7e774d8524ba8b
eljamm <fedi.jamoussi@protonmail.ch>
2024-05-02 12:21:56 +0100

    Improve keybindings for fish shell

--- nixos/hosts/default/shell/fish/init.fish
+++ nixos/hosts/default/shell/fish/init.fish
@@ -1,11 +1,14 @@
 # Empty greeter
 set fish_greeting
 
-# Enable vim-style keybinds
-fish_vi_key_bindings
-
 # Make prompt path to the following format:
 # ~/a/b/hello/world
 function fish_title
     echo (fish_prompt_pwd_dir_length=1 fish_prompt_pwd_full_dirs=2 prompt_pwd): $argv;
 end
+
+# Enable vim-style keybinds
+fish_vi_key_bindings
+
+# Make <c-f> work for vi-mode
+bind -M insert \cf forward-char
