285b557a86993a4aed929a3f8620fb4a30c404ec
eljamm <fedi.jamoussi@protonmail.ch>
2024-06-22 00:37:39 +0100

    logseq: use electron instead of insecure electron_27

--- configuration.nix
+++ configuration.nix
@@ -145,7 +145,7 @@ in
         };
         postFixup = ''
           # set the env "LOCAL_GIT_DIRECTORY" for dugite so that we can use the git in nixpkgs
-          makeWrapper ${prev.electron_27}/bin/electron $out/bin/${old.pname} \
+          makeWrapper ${prev.electron}/bin/electron $out/bin/${old.pname} \
             --set "LOCAL_GIT_DIRECTORY" ${prev.git} \
             --add-flags $out/share/${old.pname}/resources/app \
             --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}" \
