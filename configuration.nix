368b5323c2d32cf3008e5928657b297804ab698a
eljamm <fedi.jamoussi@protonmail.ch>
2024-06-22 08:45:06 +0100

    catppuccin-gtk: fix broken theme
    https://github.com/NixOS/nixpkgs/pull/321293

--- configuration.nix
+++ configuration.nix
@@ -153,6 +153,21 @@ in
         '';
       });
     })
+
+    # HACK: Fix GTK theme
+    # https://github.com/NixOS/nixpkgs/pull/321293
+    (final: prev: {
+      catppuccin-gtk = prev.catppuccin-gtk.overrideAttrs (old: {
+        src = prev.fetchFromGitHub {
+          owner = "catppuccin";
+          repo = "gtk";
+          rev = "v${old.version}";
+          fetchSubmodules = true;
+          hash = "sha256-q5/VcFsm3vNEw55zq/vcM11eo456SYE5TQA3g2VQjGc=";
+        };
+        postUnpack = "";
+      });
+    })
   ];
 
   # CCache
