b985af2aca08041036dcef7e27b0447cae271ac4
eljamm <fedi.jamoussi@protonmail.ch>
2024-07-22 09:01:25 +0100

    feat: init audio category & move musnix module

--- flake.nix
+++ flake.nix
@@ -83,7 +83,6 @@
             inputs.agenix.nixosModules.default
             inputs.catppuccin.nixosModules.catppuccin
             inputs.lix-module.nixosModules.default
-            inputs.musnix.nixosModules.musnix
             inputs.nixos-cosmic.nixosModules.default
             inputs.self.nixosModules.default
             inputs.self.nixosModules.home-manager
