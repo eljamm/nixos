87dcaeefb3e0bd2b66ab58365b5c5d28bf820baa
eljamm <fedi.jamoussi@protonmail.ch>
2024-07-30 09:02:45 +0100

    feat:replace builtins.readFile with lib.readFile

--- hosts/default/terminals/wezterm/default.nix
+++ hosts/default/terminals/wezterm/default.nix
@@ -1,7 +1,7 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   programs.wezterm = {
     enable = true;
-    extraConfig = builtins.readFile ./wezterm.lua;
+    extraConfig = lib.readFile ./wezterm.lua;
   };
 }
