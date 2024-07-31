b9cb51cfe922715465bee784ac3bb8171e37e712
eljamm <fedi.jamoussi@protonmail.ch>
2024-07-31 10:40:14 +0100

    overlays: refactor logseq

--- overlays/fixups.nix
+++ overlays/fixups.nix
@@ -3,22 +3,24 @@
 {
   nixpkgs.overlays = [
     (_: prev: {
-      logseq = prev.logseq.overrideAttrs (finalAttrs: rec {
-        version = "0.10.9";
-        src = pkgs.fetchurl {
-          url = "https://github.com/logseq/logseq/releases/download/${version}/logseq-linux-x64-${version}.AppImage";
-          hash = "sha256-XROuY2RlKnGvK1VNvzauHuLJiveXVKrIYPppoz8fCmc=";
-          name = "${finalAttrs.pname}-${version}.AppImage";
-        };
-        postFixup = ''
-              # set the env "LOCAL_GIT_DIRECTORY" for dugite so that we can use the git in nixpkgs
-          makeWrapper ${prev.electron}/bin/electron $out/bin/${finalAttrs.pname} \
+      logseq = prev.logseq.overrideAttrs (
+        finalAttrs: oldAttrs: {
+          version = "0.10.9";
+          src = pkgs.fetchurl {
+            url = "https://github.com/logseq/logseq/releases/download/${finalAttrs.version}/logseq-linux-x64-${finalAttrs.version}.AppImage";
+            hash = "sha256-XROuY2RlKnGvK1VNvzauHuLJiveXVKrIYPppoz8fCmc=";
+            name = "${finalAttrs.pname}-${finalAttrs.version}.AppImage";
+          };
+          postFixup = ''
+            # set the env "LOCAL_GIT_DIRECTORY" for dugite so that we can use the git in nixpkgs
+            makeWrapper ${prev.electron}/bin/electron $out/bin/${oldAttrs.pname} \
               --set "LOCAL_GIT_DIRECTORY" ${prev.git} \
-              --add-flags $out/share/${finalAttrs.pname}/resources/app \
-                  --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}" \
-                  --prefix LD_LIBRARY_PATH : "${prev.lib.makeLibraryPath [ prev.stdenv.cc.cc.lib ]}"
-        '';
-      });
+              --add-flags $out/share/${oldAttrs.pname}/resources/app \
+              --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}" \
+              --prefix LD_LIBRARY_PATH : "${prev.lib.makeLibraryPath [ prev.stdenv.cc.cc.lib ]}"
+          '';
+        }
+      );
     })
   ];
 }
