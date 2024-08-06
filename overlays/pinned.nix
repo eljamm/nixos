237c59ccdf94b3890e8f1061cacf48268fe3e8f3
eljamm <fedi.jamoussi@protonmail.ch>
2024-08-06 23:10:04 +0100

    taskwarrior: 2.6.2 -> 3.1.0

--- overlays/pinned.nix
+++ overlays/pinned.nix
@@ -37,5 +37,28 @@
         )).override
           { cudaSupport = true; };
     })
+
+    # taskwarrior3
+    (_: prev: {
+      taskwarrior3 = prev.taskwarrior3.overrideAttrs rec {
+        pname = "taskwarrior";
+        version = "3.1.0";
+        src = prev.fetchFromGitHub {
+          owner = "GothenburgBitFactory";
+          repo = "taskwarrior";
+          rev = "v${version}";
+          hash = "sha256-iKpOExj1xM9rU/rIcOLLKMrZrAfz7y9X2kt2CjfMOOQ=";
+          fetchSubmodules = true;
+        };
+        preCheck = "";
+        checkTarget = "build_tests";
+        cargoDeps = prev.rustPlatform.fetchCargoTarball {
+          name = "${pname}-${version}-cargo-deps";
+          inherit src;
+          sourceRoot = src.name;
+          hash = "sha256-L+hYYKXSOG4XYdexLMG3wdA7st+A9Wk9muzipSNjxrA=";
+        };
+      };
+    })
   ];
 }
