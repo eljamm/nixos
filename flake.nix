832e772a5c54c4e9c154a83a62dbefde644d610b
eljamm <fedi.jamoussi@protonmail.ch>
2024-08-17 11:06:35 +0100

    flake: unstable -> unstable-small & update inputs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
 
     nixpkgs-system.url = "github:eljamm/nixpkgs/system";
 
