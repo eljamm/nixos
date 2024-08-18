1bd8d00d7d10225f50e8124c1a3b54eaef301418
eljamm <fedi.jamoussi@protonmail.ch>
2024-08-18 11:57:58 +0100

    gnome: test workaround to prevent crashing

--- modules/nixos/desktop/gnome.nix
+++ modules/nixos/desktop/gnome.nix
@@ -64,6 +64,10 @@
       __EGL_VENDOR_LIBRARY_FILENAMES = "${pkgs.mesa.drivers.outPath}/share/glvnd/egl_vendor.d/50_mesa.json";
       __GLX_VENDOR_LIBRARY_NAME = "mesa";
       VK_DRIVER_FILES = "/run/opengl-driver/share/vulkan/icd.d/radeon_icd.x86_64.json";
+
+      # gnome-shell randomly crashes without this
+      # https://gitlab.gnome.org/GNOME/mutter/-/issues/3358
+      MUTTER_DEBUG_KMS_THREAD_TYPE = "user";
     };
 
   };
