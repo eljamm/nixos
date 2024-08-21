81129aae31dd0433d1aed0cb9954e6ba85ad9be2
eljamm <fedi.jamoussi@protonmail.ch>
2024-08-21 18:39:07 +0100

    nvidia: 555.58.02 -> 560.35.03, cleanup module

--- hardware/legion/nvidia.nix
+++ hardware/legion/nvidia.nix
@@ -24,9 +24,6 @@ in
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = [ "nvidia" ];
 
-  # Loading nvidia-uvm is needed to make CUDA work with nvidia-open
-  boot.kernelModules = lib.optionals config.hardware.nvidia.open [ "nvidia_uvm" ];
-
   environment.systemPackages = [ nvidia-offload ];
 
   hardware.nvidia = {
@@ -67,6 +64,14 @@ in
       nvidiaBusId = "PCI:1:0:0";
     };
 
-    package = config.boot.kernelPackages.nvidiaPackages.stable; # 555.58.02
+    # stable
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "560.35.03";
+      sha256_64bit = "sha256-8pMskvrdQ8WyNBvkU/xPc/CtcYXCa7ekP73oGuKfH+M=";
+      sha256_aarch64 = "sha256-s8ZAVKvRNXpjxRYqM3E5oss5FdqW+tv1qQC2pDjfG+s=";
+      openSha256 = "sha256-/32Zf0dKrofTmPZ3Ratw4vDM7B+OgpC4p7s+RHUjCrg=";
+      settingsSha256 = "sha256-kQsvDgnxis9ANFmwIwB7HX5MkIAcpEEAHc8IBOLdXvk=";
+      persistencedSha256 = "sha256-E2J2wYYyRu7Kc3MMZz/8ZIemcZg68rkzvqEwFAL3fFs=";
+    };
   };
 }
