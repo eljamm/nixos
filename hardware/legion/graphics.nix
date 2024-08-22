cc15d009a37e99713e851f1eb0d2f56794c487b8
eljamm <fedi.jamoussi@protonmail.ch>
2024-08-22 21:35:46 +0100

    feat: remove iGPU env vars & add amd-offload
    - To use an external screen, the GNOME shell needs to run from the
      dedicated gpu
    - Add `amd-offload` command to force programs to use the integrated gpu

--- hardware/legion/graphics.nix
+++ hardware/legion/graphics.nix
@@ -1,5 +1,17 @@
 { pkgs, ... }:
+
+let
+  amd-offload = pkgs.writeShellScriptBin "amd-offload" ''
+    export __EGL_VENDOR_LIBRARY_FILENAMES="${pkgs.mesa.drivers.outPath}/share/glvnd/egl_vendor.d/50_mesa.json"
+    export __GLX_VENDOR_LIBRARY_NAME="mesa"
+    export VK_DRIVER_FILES="${pkgs.mesa.drivers.outPath}/share/vulkan/icd.d/radeon_icd.x86_64.json"
+    exec "$@"
+  '';
+in
+
 {
+  environment.systemPackages = [ amd-offload ];
+
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
