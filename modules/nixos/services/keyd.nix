1ec96b1f8bdbc1f8d2f8b2c9312da670388c5e67
eljamm <fedi.jamoussi@protonmail.ch>
2024-08-24 22:55:58 +0100

    keyd: make common settings & add dierya keyboard

--- modules/nixos/services/keyd.nix
+++ modules/nixos/services/keyd.nix
@@ -1,48 +1,75 @@
-_: {
+{ lib, ... }:
+let
+  # NOTE: valid key names: `keyd list-keys`
+  commonSettings = {
+    main = {
+      capslock = "esc";
+      insert = "S-insert";
+    };
+    altgr = {
+      capslock = "backspace";
+
+      # Vim
+      k = "up";
+      l = "right";
+      j = "down";
+      h = "left";
+      x = "C-w"; # close
+
+      # Screenshot
+      i = "sysrq";
+
+      g = "home";
+    };
+    "altgr+shift" = {
+      j = "pagedown";
+      k = "pageup";
+
+      # Media keys
+      a = "previoussong";
+      d = "nextsong";
+
+      e = "volumeup";
+      q = "volumedown";
+
+      m = "mute";
+      s = "playpause";
+
+      f = "rfkill";
+
+      g = "end";
+    };
+    "control+shift" = {
+      # Next/Previous Tabs
+      j = "C-S-tab";
+      k = "C-tab";
+    };
+  };
+in
+{
   services.keyd = {
     enable = true;
     keyboards = {
-      default = {
-        ids = [ "*" ];
-        settings = {
-          # NOTE: valid key names: `keyd list-keys`
+      dierya = {
+        ids = [ "1a2c:95f6" ];
+        settings = lib.recursiveUpdate commonSettings {
           main = {
-            # Remap caps to `escape`
-            capslock = "esc";
-
-            # Newline on enter
-            insert = "S-insert";
+            esc = "^";
           };
           altgr = {
-            capslock = "backspace";
-
-            # Vim
-            k = "up";
-            l = "right";
-            j = "down";
-            h = "left";
-            x = "C-w"; # close
-
-            # Screenshot
-            i = "sysrq";
+            r = "macro(leftalt+home)";
           };
-          "altgr+shift" = {
-            j = "pagedown";
-            k = "pageup";
-
-            # Media keys
-            a = "previoussong";
-            d = "nextsong";
-
-            e = "volumeup";
-            q = "volumedown";
-
-            m = "mute";
-            s = "playpause";
-
-            f = "rfkill";
+          control = {
+            esc = "`";
+          };
+          "control+alt" = {
+            esc = "~";
           };
         };
+      };
+      default = {
+        ids = [ "*" ];
+        settings = commonSettings;
       };
     };
   };
