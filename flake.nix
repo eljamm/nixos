6adfed76babc3c5b0c52fcbda78b8ca597bde25f
eljamm <fedi.jamoussi@protonmail.ch>
2024-08-27 10:35:31 +0100

    flake: small refactor

--- flake.nix
+++ flake.nix
@@ -81,16 +81,14 @@
 
       nixosConfigurations = {
         nixos = nixpkgs.lib.nixosSystem {
-          specialArgs = {
-            inherit inputs;
-          };
+          specialArgs.inputs = inputs;
           modules = [
             ./configuration.nix
             ./modules/nixos/home-manager.nix
             inputs.agenix.nixosModules.default
             inputs.catppuccin.nixosModules.catppuccin
             inputs.lix-module.nixosModules.default
-            inputs.self.nixosModules.default
+            self.nixosModules.default
             { nixpkgs.overlays = overlays; }
           ];
         };
