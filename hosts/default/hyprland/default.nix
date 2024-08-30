a7cdc8d19de2094b1a2475e2ccac4ff96f6d5e90
eljamm <fedi.jamoussi@protonmail.ch>
2024-08-30 01:13:05 +0100

    hyprland: init hypridle

--- hosts/default/hyprland/default.nix
+++ hosts/default/hyprland/default.nix
@@ -18,4 +18,36 @@
     settings = pkgs.callPackage ./config.nix;
     plugins = with pkgs.hyprlandPlugins; [ hy3 ];
   };
+
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+        ignore_dbus_inhibit = false;
+        lock_cmd = "hyprlock";
+      };
+      listener = [
+        # Monitor backlight
+        {
+          timeout = 300;
+          # set backlight to minimum, avoid 0 on OLED monitor
+          on-timeout = "brightnessctl -s set 10";
+          on-resume = "brightnessctl -r";
+        }
+        # Keyboard backlight
+        {
+          timeout = 300;
+          on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0 ";
+          on-resume = "brightnessctl -rd rgb:kbd_backlight";
+        }
+        # Screen off after timeout, on when activity is detected
+        {
+          timeout = 900;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+      ];
+    };
+  };
 }
