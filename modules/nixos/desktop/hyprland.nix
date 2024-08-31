09dfd849a3879c87e5b40228ed51cd90ba0f8577
eljamm <fedi.jamoussi@protonmail.ch>
2024-08-31 20:53:37 +0100

    hyprland: add packages more packages

--- modules/nixos/desktop/hyprland.nix
+++ modules/nixos/desktop/hyprland.nix
@@ -39,10 +39,29 @@ in
     waybar
 
     swww
+
+    ddcutil
+    iio-hyprland
+    wluma
   ];
 
   nix.settings = lib.mkIf hyprlandEnabled {
     substituters = [ "https://hyprland.cachix.org" ];
     trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
   };
+
+  boot.kernelModules = [
+    # "ddcci"
+    # "ddcci-backlight"
+    "i2c-dev"
+  ];
+
+  # boot.extraModulePackages = [ config.boot.kernelPackages.ddcci-driver ];
+
+  # systemd.services.ddcci = {
+  #   serviceConfig.Type = "oneshot";
+  #   script = ''
+  #     echo 'ddcci 0x37' > /sys/bus/i2c/devices/i2c-2/new_device
+  #   '';
+  # };
 }
