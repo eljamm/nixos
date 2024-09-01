c04508cc56dc2309654764ace8aaa69d5c9cc0a2
eljamm <fedi.jamoussi@protonmail.ch>
2024-09-01 22:57:11 +0100

    feat: init niri, a scrolling wayland compositor

--- flake.nix
+++ flake.nix
@@ -41,6 +41,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    niri = {
+      url = "github:sodiboo/niri-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     spicetify-nix = {
       url = "github:Gerg-L/spicetify-nix";
       inputs.nixpkgs.follows = "nixpkgs";
