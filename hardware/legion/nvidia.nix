3fffe4507af6690c779eb03376f1bd121784c914
eljamm <fedi.jamoussi@protonmail.ch>
2024-09-03 00:49:32 +0100

    nvidia: use nouveau, add proprietary specialisation

--- hardware/legion/nvidia.nix
+++ hardware/legion/nvidia.nix
@@ -1,5 +1,6 @@
 {
   config,
+  lib,
   pkgs,
   ...
 }:
@@ -18,60 +19,82 @@ let
   '';
 in
 {
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = [ "nvidia" ];
-
-  environment.systemPackages = [
-    nvidia-offload
-    pkgs.nvitop
+  boot.kernelParams = [
+    # Fix Wayland flickering & AMD-Vi IO_PAGE_FAULT errors
+    # https://gitlab.freedesktop.org/drm/nouveau/-/issues/225
+    "iommu=pt"
   ];
 
-  hardware.nvidia = {
-    # Modesetting is required.
-    modesetting.enable = true;
+  # Separate boot entry for the proprietary Nvidia drivers
+  # NOTE: nouveau is used in the default boot entry
+  specialisation = {
+    prop-nvidia.configuration = {
+      system.nixos.tags = [ "proprietary-nvidia" ];
 
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-    # of just the bare essentials.
-    powerManagement.enable = true;
+      # Load nvidia driver for Xorg and Wayland
+      services.xserver.videoDrivers = [ "nvidia" ];
 
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = true;
+      environment.systemPackages = [
+        nvidia-offload
+        pkgs.nvitop
+      ];
 
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    # NOTE: Enabled by default from driver 560.28.03+
-    open = true;
+      environment.sessionVariables = {
+        # Use integrated GPU for gnome-shell
+        # See https://gitlab.gnome.org/GNOME/mutter/-/issues/2969
+        __EGL_VENDOR_LIBRARY_FILENAMES = "${pkgs.mesa.drivers.outPath}/share/glvnd/egl_vendor.d/50_mesa.json";
+        __GLX_VENDOR_LIBRARY_NAME = "mesa";
+        VK_DRIVER_FILES = "/run/opengl-driver/share/vulkan/icd.d/radeon_icd.x86_64.json";
+      };
 
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
+      hardware.nvidia = {
+        # Modesetting is required.
+        modesetting.enable = true;
 
-    prime = {
-      offload.enable = true;
-      reverseSync.enable = true;
+        # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+        # Enable this if you have graphical corruption issues or application crashes after waking
+        # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+        # of just the bare essentials.
+        powerManagement.enable = true;
 
-      # Enable if using an external GPU
-      allowExternalGpu = false;
+        # Fine-grained power management. Turns off GPU when not in use.
+        # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+        powerManagement.finegrained = false;
 
-      amdgpuBusId = "PCI:52:0:0";
-      nvidiaBusId = "PCI:1:0:0";
-    };
+        # Use the NVidia open source kernel module (not to be confused with the
+        # independent third-party "nouveau" open source driver).
+        # Support is limited to the Turing and later architectures. Full list of 
+        # supported GPUs is at: 
+        # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+        # Only available from driver 515.43.04+
+        # NOTE: Enabled by default from driver 560.28.03+
+        open = true;
 
-    # stable
-    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "560.35.03";
-      sha256_64bit = "sha256-8pMskvrdQ8WyNBvkU/xPc/CtcYXCa7ekP73oGuKfH+M=";
-      sha256_aarch64 = "sha256-s8ZAVKvRNXpjxRYqM3E5oss5FdqW+tv1qQC2pDjfG+s=";
-      openSha256 = "sha256-/32Zf0dKrofTmPZ3Ratw4vDM7B+OgpC4p7s+RHUjCrg=";
-      settingsSha256 = "sha256-kQsvDgnxis9ANFmwIwB7HX5MkIAcpEEAHc8IBOLdXvk=";
-      persistencedSha256 = "sha256-E2J2wYYyRu7Kc3MMZz/8ZIemcZg68rkzvqEwFAL3fFs=";
+        # Enable the Nvidia settings menu,
+        # accessible via `nvidia-settings`.
+        nvidiaSettings = true;
+
+        prime = {
+          sync.enable = true;
+
+          # Enable if using an external GPU
+          allowExternalGpu = false;
+
+          amdgpuBusId = "PCI:52:0:0";
+          nvidiaBusId = "PCI:1:0:0";
+        };
+
+        # stable
+        package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+          version = "560.35.03";
+          sha256_64bit = "sha256-8pMskvrdQ8WyNBvkU/xPc/CtcYXCa7ekP73oGuKfH+M=";
+          sha256_aarch64 = "sha256-s8ZAVKvRNXpjxRYqM3E5oss5FdqW+tv1qQC2pDjfG+s=";
+          openSha256 = "sha256-/32Zf0dKrofTmPZ3Ratw4vDM7B+OgpC4p7s+RHUjCrg=";
+          settingsSha256 = "sha256-kQsvDgnxis9ANFmwIwB7HX5MkIAcpEEAHc8IBOLdXvk=";
+          persistencedSha256 = "sha256-E2J2wYYyRu7Kc3MMZz/8ZIemcZg68rkzvqEwFAL3fFs=";
+        };
+      };
     };
   };
+
 }
