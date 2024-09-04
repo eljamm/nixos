b2f2ea3871792ad827f26771a670a838e519a3b9
eljamm <fedi.jamoussi@protonmail.ch>
2024-09-04 12:03:21 +0100

    amdgpu: use zenpower sensor

--- hardware/legion/graphics.nix
+++ hardware/legion/graphics.nix
@@ -1,6 +1,5 @@
 {
-  inputs,
-  lib,
+  config,
   pkgs,
   ...
 }:
@@ -17,9 +16,11 @@ in
   # WIP: has problems with amdgpu
   # chaotic.mesa-git.enable = true;
 
-  # Load amdgpu driver for Xorg and Wayland
-  # NOTE: probably useless to do, but oh well
-  services.xserver.videoDrivers = lib.mkBefore [ "amdgpu" ];
+  # Enables the zenpower sensor in lieu of the k10temp sensor on Zen CPUs https://git.exozy.me/a/zenpower3
+  # On Zen CPUs zenpower produces much more data entries
+  boot.blacklistedKernelModules = [ "k10temp" ];
+  boot.extraModulePackages = [ config.boot.kernelPackages.zenpower ];
+  boot.kernelModules = [ "zenpower" ];
 
   hardware.graphics = {
     enable = true;
