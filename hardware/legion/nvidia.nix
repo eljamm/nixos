2f8d88a7c1e734f8230062f61c2be659e6b3ab04
eljamm <fedi.jamoussi@protonmail.ch>
2024-09-10 12:45:32 +0100

    nvidia: use `lib.concatStringSep` instead of builtins

--- hardware/legion/nvidia.nix
+++ hardware/legion/nvidia.nix
@@ -14,7 +14,7 @@ let
     export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
     export __GLX_VENDOR_LIBRARY_NAME=nvidia
     export __VK_LAYER_NV_optimus=NVIDIA_only
-    export VK_DRIVER_FILES="${builtins.concatStringsSep ":" vulkanDriverFiles}"
+    export VK_DRIVER_FILES="${lib.concatStringsSep ":" vulkanDriverFiles}"
     exec "$@"
   '';
   amd-offload = pkgs.writeShellScriptBin "amd-offload" ''
