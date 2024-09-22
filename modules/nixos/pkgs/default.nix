9f0fe14112281c51c384d1015c824d201e4ae722
eljamm <fedi.jamoussi@protonmail.ch>
2024-09-22 11:16:03 +0100

    chore: temporarily fix redlib

--- modules/nixos/pkgs/default.nix
+++ modules/nixos/pkgs/default.nix
@@ -3,5 +3,7 @@ with pkgs;
 {
   nixpkgs.overlays = [
     (final: prev: { cinny-desktop = callPackage ./ci/cinny-desktop/package.nix { }; })
+    # FIX: wait for next release
+    (final: prev: { redlib = callPackage ./re/redlib/package.nix { }; })
   ];
 }
