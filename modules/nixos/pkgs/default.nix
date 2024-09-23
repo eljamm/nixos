d8dd0412b1c8b099fe415af6cc27a94d68709479
eljamm <fedi.jamoussi@protonmail.ch>
2024-09-23 12:24:39 +0100

    WIP: pin kitty

--- modules/nixos/pkgs/default.nix
+++ modules/nixos/pkgs/default.nix
@@ -5,5 +5,11 @@ with pkgs;
     (final: prev: { cinny-desktop = callPackage ./ci/cinny-desktop/package.nix { }; })
     # FIX: wait for next release
     (final: prev: { redlib = callPackage ./re/redlib/package.nix { }; })
+    (final: prev: {
+      kitty = callPackage ./ki/kitty/package.nix {
+        buildGoModule = pkgs.buildGo123Module;
+        go = go_1_23;
+      };
+    })
   ];
 }
