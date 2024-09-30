42c35a48491d77366a5d880307f0118cbedef7e7
eljamm <fedi.jamoussi@protonmail.ch>
2024-09-30 16:23:46 +0100

    feat: use nixos-hardware modules

--- hardware/legion/default.nix
+++ hardware/legion/default.nix
@@ -1,14 +1,20 @@
 {
   config,
+  inputs,
   pkgs,
   ...
 }:
 {
   imports = [
     ./filesystems.nix
+    ./graphics.nix
     ./nvidia.nix
     ./virtual-camera.nix
-    ./graphics.nix
+    inputs.nixos-hardware.nixosModules.common-cpu-amd
+    inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
+    inputs.nixos-hardware.nixosModules.common-cpu-amd-zenpower
+    inputs.nixos-hardware.nixosModules.common-pc-laptop
+    inputs.nixos-hardware.nixosModules.common-pc-laptop-ssd
   ];
 
   boot.kernelModules = [ "legion-laptop" ];
--- hardware/legion/graphics.nix
+++ hardware/legion/graphics.nix
@@ -23,11 +23,7 @@ in
   # WIP: has problems with amdgpu
   # chaotic.mesa-git.enable = true;
 
-  # Enables the zenpower sensor in lieu of the k10temp sensor on Zen CPUs https://git.exozy.me/a/zenpower3
-  # On Zen CPUs zenpower produces much more data entries
-  boot.blacklistedKernelModules = [ "k10temp" ];
-  boot.extraModulePackages = [ config.boot.kernelPackages.zenpower ];
-  boot.kernelModules = [ "zenpower" ];
+  hardware.amdgpu.initrd.enable = false;
 
   hardware.graphics = {
     enable = true;
