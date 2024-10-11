eb2e5043ca071a6f9094cbcdaa23474204e39ed6
eljamm <fedi.jamoussi@protonmail.ch>
2024-10-11 09:59:35 +0100

    feat: add ccache options

--- modules/nixos/tools/ccache.nix
+++ modules/nixos/tools/ccache.nix
@@ -1,9 +1,11 @@
-{ config, lib, ... }:
-
+{
+  config,
+  lib,
+  ...
+}:
 let
   ccacheEnabled = config.programs.ccache.enable;
 in
-
 {
   # Incremental Builds
   programs.ccache.enable = lib.mkDefault true;
@@ -12,9 +14,12 @@ in
     (_: super: {
       ccacheWrapper = super.ccacheWrapper.override {
         extraConfig = ''
-          export CCACHE_COMPRESS=1
           export CCACHE_DIR="${config.programs.ccache.cacheDir}"
           export CCACHE_UMASK=007
+          export CCACHE_NOCOMPRESS=true
+          export CCACHE_MAXSIZE=10G
+          export CCACHE_SLOPPINESS=random_seed
+          export CCACHE_BASEDIR="$NIX_BUILD_TOP"
           if [ ! -d "$CCACHE_DIR" ]; then
             echo "====="
             echo "Directory '$CCACHE_DIR' does not exist"
