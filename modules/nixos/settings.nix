dd4dfaa4e273fc1fdbfe2670dba8c5150c5fa357
eljamm <fedi.jamoussi@protonmail.ch>
2024-10-11 10:21:21 +0100

    feat: remove devenv cache

--- modules/nixos/settings.nix
+++ modules/nixos/settings.nix
@@ -13,14 +13,8 @@
 
     settings = {
       # Cache
-      substituters = [
-        "https://nix-community.cachix.org"
-        "https://devenv.cachix.org"
-      ];
-      trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-      ];
+      substituters = [ "https://nix-community.cachix.org" ];
+      trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
 
       warn-dirty = false; # NOTE: I do not care.
 
