c3c5373fabc685af951fcf51a7360f7d84e8c421
eljamm <fedi.jamoussi@protonmail.ch>
2024-10-23 16:14:47 +0100

    albert: 0.26.5 -> 0.26.6

--- hosts/nixos/overlays/pinned.nix
+++ hosts/nixos/overlays/pinned.nix
@@ -5,12 +5,12 @@
     # albert launcher
     (final: prev: {
       albert = prev.albert.overrideAttrs rec {
-        version = "0.26.5";
+        version = "0.26.6";
         src = final.fetchFromGitHub {
           owner = "albertlauncher";
           repo = "albert";
           rev = "v${version}";
-          hash = "sha256-0ONnG3BOqHa0KqjqvGVEflYqtpo3Wt4duMBDXXHUZEk=";
+          hash = "sha256-Z4YgqqtJPYMzpnMt74TX2Hi0AEMyhRc2QHSVuwuaxfE=";
           fetchSubmodules = true;
         };
       };
