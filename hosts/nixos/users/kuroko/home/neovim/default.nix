1c14c11891000ab5e98c1f85f4ab119b25090610
eljamm <fedi.jamoussi@protonmail.ch>
2024-10-23 23:38:47 +0100

    feat: refactor neovim module

--- hosts/nixos/users/kuroko/home/neovim/default.nix
+++ hosts/nixos/users/kuroko/home/neovim/default.nix
@@ -1,122 +1,103 @@
 {
   pkgs,
+  lib,
   ...
 }:
-with pkgs;
 let
-  tools = [
-    fswatch # File watcher utility, replacing libuv.fs_event for neovim 10.0
-    fzf
-    git
-    sqlite
-    tree-sitter
-  ];
+  packages = with pkgs; {
+    tools = [
+      fswatch # File watcher utility, replacing libuv.fs_event for neovim 10.0
+      fzf
+      git
+      sqlite
+      tree-sitter
+    ];
 
-  c = [
-    # clang # TODO: remove. big closure
-    # clang-tools # TODO: remove. big closure
-    cmake
-    gcc
-    gnumake
-  ];
+    c = [
+      # clang # TODO: remove. big closure
+      # clang-tools # TODO: remove. big closure
+      cmake
+      gcc
+      gnumake
+    ];
 
-  gamedev = [
-    # parser, linter and formatter for GDScript
-    gdtoolkit_4
-  ];
+    gamedev = [
+      # parser, linter and formatter for GDScript
+      gdtoolkit_4
+    ];
 
-  golang = [
-    delve # debugger
-    go
-    gofumpt
-    goimports-reviser
-    golines
-    gopls
-    gotools
-  ];
+    golang = [
+      delve # debugger
+      go
+      gofumpt
+      goimports-reviser
+      golines
+      gopls
+      gotools
+    ];
 
-  # TODO: big closure. should be enabled per-project
-  haskell = [
-    haskell-language-server
-    ghc
-  ];
+    luaTools = [
+      lua-language-server
+      lua51Packages.lua
+      lua51Packages.luarocks-nix
+      stylua
+    ];
 
-  luaTools = [
-    lua-language-server
-    lua51Packages.lua
-    lua51Packages.luarocks-nix
-    stylua
-  ];
+    markup = [
+      cbfmt # format codeblocks
+      codespell
+      nodePackages.cspell
+      markdownlint-cli
+      # TODO: mdformat plugins don't work outside of dev shells
+      (mdformat.withPlugins (
+        ps: with ps; [
+          mdformat-footnote
+          mdformat-frontmatter
+          mdformat-tables
+          mdformat-toc
+        ]
+      ))
+      # plantuml # TODO: remove. big closure
+    ];
 
-  markup = [
-    cbfmt # format codeblocks
-    codespell
-    nodePackages.cspell
-    markdownlint-cli
-    # TODO: mdformat plugins don't work outside of dev shells
-    (mdformat.withPlugins (
-      ps: with ps; [
-        mdformat-footnote
-        mdformat-frontmatter
-        mdformat-tables
-        mdformat-toc
-      ]
-    ))
-    # typst-lsp # FIX: broken
-    # plantuml # TODO: remove. big closure
-  ];
+    nix = [
+      deadnix
+      manix
+      nil
+      nixfmt-rfc-style
+      nixpkgs-lint-community
+      statix
+    ];
 
-  nix = [
-    deadnix
-    manix
-    nil
-    nixfmt-rfc-style
-    nixpkgs-lint-community
-    statix
-  ];
+    python = [
+      black
+      isort
+      python311Packages.jedi-language-server
+      ruff
+      ruff-lsp
+    ];
 
-  python = [
-    black
-    isort
-    python311Packages.jedi-language-server
-    ruff
-    ruff-lsp
-  ];
+    rust = [
+      bacon # background code check
+      taplo # TOML
+    ];
 
-  rust = [
-    bacon # background code check
-    taplo # TOML
-  ];
+    shell = [
+      nodePackages.bash-language-server
+      shellcheck
+      shfmt
+    ];
 
-  shell = [
-    nodePackages.bash-language-server
-    shellcheck
-    shfmt
-  ];
-
-  web = [
-    deno
-    nodePackages.sql-formatter
-    nodePackages.typescript-language-server
-    nodejs
-    prettierd # multi-language formatters
-    vscode-langservers-extracted
-    yarn
-  ];
-
-  extraPackages =
-    tools
-    ++ c
-    ++ gamedev
-    ++ golang
-    # ++ haskell # TODO: remove (enable per-project)
-    ++ luaTools
-    # ++ markup # TODO: remove (already enabled system-wide)
-    ++ nix
-    ++ python
-    # ++ rust # TODO: remove (already enabled system-wide)
-    ++ shell
-    ++ web;
+    web = [
+      deno
+      nodePackages.sql-formatter
+      nodePackages.typescript-language-server
+      nodejs
+      prettierd # multi-language formatters
+      vscode-langservers-extracted
+      yarn
+    ];
+  };
 in
 {
   # HACK: manix
@@ -124,22 +105,19 @@ in
   manual.json.enable = true;
 
   # for quick development
-  home.packages = rust ++ markup;
+  home.packages = with packages; rust ++ markup;
 
   programs.neovim = {
     enable = true;
     defaultEditor = true;
-    package = neovim-unwrapped;
-    plugins = with vimPlugins; [
+    package = pkgs.neovim-unwrapped;
+    plugins = with pkgs.vimPlugins; [
       neorg
       telescope-cheat-nvim
     ];
-    inherit extraPackages;
-  };
-
-  # NOTE: unused
-  programs.helix = {
-    enable = false;
-    inherit extraPackages;
+    extraPackages = lib.pipe packages [
+      (lib.mapAttrsToList (name: value: value))
+      lib.flatten
+    ];
   };
 }
