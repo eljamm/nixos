68c6c31899870d7d6c7344aa867fa164ad9f91cf
eljamm <fedi.jamoussi@protonmail.ch>
2024-11-05 12:15:05 +0100

    refactor(hosts): leverage flake-parts

--- flake/hosts.nix
+++ flake/hosts.nix
@@ -1,36 +1,42 @@
-{ self, inputs, ... }:
-let
-  system = "x86_64-linux";
-  pkgs = inputs.nixpkgs.legacyPackages.${system};
-in
 {
-  flake.nixosConfigurations = {
-    nixos = inputs.nixpkgs.lib.nixosSystem {
-      specialArgs = {
-        inherit self inputs;
-        pkgsCustom = inputs.nixpkgs-stable.legacyPackages.${system};
+  inputs,
+  self,
+  withSystem,
+  ...
+}:
+{
+  flake = withSystem "x86_64-linux" (
+    ctx@{ inputs', ... }:
+    {
+      nixosConfigurations = {
+        nixos = inputs.nixpkgs.lib.nixosSystem {
+          specialArgs = {
+            inherit self inputs inputs';
+            pkgsCustom = inputs'.nixpkgs-stable.legacyPackages;
+          };
+          modules = [
+            ../hosts/nixos
+            ../modules/nixos
+            inputs.agenix.nixosModules.default
+            inputs.catppuccin.nixosModules.catppuccin
+            inputs.chaotic.nixosModules.default
+            {
+              # TODO: is nix-alien needed?
+              nixpkgs.overlays = [ inputs.nix-alien.overlays.default ];
+              nixpkgs.config.allowUnfree = true;
+            }
+          ];
+        };
       };
-      modules = [
-        ../hosts/nixos
-        ../modules/nixos
-        inputs.agenix.nixosModules.default
-        inputs.catppuccin.nixosModules.catppuccin
-        inputs.chaotic.nixosModules.default
-        {
-          # TODO: is nix-alien needed?
-          nixpkgs.overlays = [ inputs.nix-alien.overlays.default ];
-          nixpkgs.config.allowUnfree = true;
-        }
-      ];
-    };
-  };
 
-  flake.homeConfigurations.kuroko = inputs.home-manager.lib.homeManagerConfiguration {
-    inherit pkgs;
-    modules = [ ../hosts/nixos/users/kuroko/home/default.nix ];
-    extraSpecialArgs = {
-      inherit inputs;
-      pkgsCustom = inputs.nixpkgs-stable.legacyPackages.${system};
-    };
-  };
+      homeConfigurations.kuroko = inputs.home-manager.lib.homeManagerConfiguration {
+        pkgs = inputs'.nixpkgs.legacyPackages;
+        modules = [ ../hosts/nixos/users/kuroko/home/default.nix ];
+        extraSpecialArgs = {
+          inherit self inputs inputs';
+          pkgsCustom = inputs'.nixpkgs-stable.legacyPackages;
+        };
+      };
+    }
+  );
 }
