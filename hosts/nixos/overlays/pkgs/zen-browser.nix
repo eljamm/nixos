22c25fd39a47183fdd1ff98f85f05d7ea31f3ec7
eljamm <fedi.jamoussi@protonmail.ch>
2024-11-13 16:34:03 +0100

    zen-browser-bin: 1.0.0-1.32 -> 1.0.1-a.19

--- hosts/nixos/overlays/pkgs/zen-browser.nix
+++ hosts/nixos/overlays/pkgs/zen-browser.nix
@@ -62,11 +62,11 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "zen-browser-bin";
-  version = "1.0.0-a.32";
+  version = "1.0.1-a.19";
 
   src = fetchzip {
     url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-specific.tar.bz2";
-    hash = "sha256-IRheE/4EhK8I7aCEDmbhwSrvxISKU6e42ezAp+FnH/M=";
+    hash = "sha256-WGaRfsiewSEKK0RC5OMGp7zUbJBy2j1IPUl55XzA9rw=";
   };
 
   desktopItems = [
