af4d3a3c9903faa2848f606d7c9c194c89f1fcc2
eljamm <fedi.jamoussi@protonmail.ch>
2024-11-15 16:35:54 +0100

    feat(gnome): add KMS thread patch to avoid crashes

--- modules/nixos/desktops/gnome/default.nix
+++ modules/nixos/desktops/gnome/default.nix
@@ -103,6 +103,14 @@
                     url = "https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4114.patch";
                     hash = "sha256-MLWUKQGuhgIkgAKrVUJAc4fgPvHUaYlS79UVsTWF/TE=";
                   })
+                  # Avoid crashed by defaulting to high priority thread instead
+                  # of realtime for the KMS thread
+                  # https://www.phoronix.com/news/GNOME-High-Priority-KMS-Thread
+                  # https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4124
+                  (pkgs.fetchpatch2 {
+                    url = "https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4124.patch";
+                    hash = "sha256-h1gjyZx23NQ3VDwcGRy6hLkfgLdukao7NzH+48C/NE4=";
+                  })
                 ];
               });
             })
