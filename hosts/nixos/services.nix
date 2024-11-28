8e70a7ef6c60065590aa891a64c38332e829b721
eljamm <fedi.jamoussi@protonmail.ch>
2024-11-28 22:22:54 +0100

    refactor: move common & remove unused services

--- hosts/nixos/services.nix
+++ hosts/nixos/services.nix
@@ -1,36 +1,8 @@
+{ ... }:
 {
-  inputs,
-  config,
-  pkgs,
-  ...
-}:
-{
-  # Enable CUPS to print documents.
-  services.printing.enable = false;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # TODO: move
-  services.xserver.wacom.enable = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
-
   programs.firejail.enable = true;
 
   programs.adb.enable = true;
-
-  # https://github.com/viperML/nh
-  programs.nh = {
-    enable = true;
-    flake = config.environment.sessionVariables.FLAKE;
-  };
 
   # https://github.com/nix-community/nix-ld
   programs.nix-ld.enable = true;
--- modules/nixos/services/default.nix
+++ modules/nixos/services/default.nix
@@ -1,9 +1,24 @@
-{ ... }:
+{ config, ... }:
 {
   imports = [
     ./anki.nix
     ./blocky.nix
     ./keyd.nix
+    ./misc.nix
     ./mumble.nix
   ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # https://github.com/viperML/nh
+  programs.nh = {
+    enable = true;
+    flake = config.environment.sessionVariables.FLAKE;
+  };
 }
