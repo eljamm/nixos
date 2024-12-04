be70343b8a5a080c3ead33f66c52664751afc3a7
eljamm <fedi.jamoussi@protonmail.ch>
2024-12-04 15:41:21 +0100

    feat: remove logseq

--- hosts/nixos/overlays/fixups.nix
+++ hosts/nixos/overlays/fixups.nix
@@ -2,29 +2,6 @@
 
 {
   nixpkgs.overlays = [
-    # TODO: worth following:
-    # https://github.com/NixOS/nixpkgs/pull/340427
-    # https://github.com/NixOS/nixpkgs/pull/340128
-    (_: prev: {
-      logseq = prev.logseq.overrideAttrs (
-        finalAttrs: oldAttrs: {
-          version = "0.10.9";
-          src = pkgs.fetchurl {
-            url = "https://github.com/logseq/logseq/releases/download/${finalAttrs.version}/logseq-linux-x64-${finalAttrs.version}.AppImage";
-            hash = "sha256-XROuY2RlKnGvK1VNvzauHuLJiveXVKrIYPppoz8fCmc=";
-            name = "${finalAttrs.pname}-${finalAttrs.version}.AppImage";
-          };
-          postFixup = ''
-            # set the env "LOCAL_GIT_DIRECTORY" for dugite so that we can use the git in nixpkgs
-            makeWrapper ${prev.electron}/bin/electron $out/bin/${oldAttrs.pname} \
-              --set "LOCAL_GIT_DIRECTORY" ${prev.git} \
-              --add-flags $out/share/${oldAttrs.pname}/resources/app \
-              --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}" \
-              --prefix LD_LIBRARY_PATH : "${prev.lib.makeLibraryPath [ prev.stdenv.cc.cc.lib ]}"
-          '';
-        }
-      );
-    })
     (final: prev: {
       neovim-unwrapped = prev.neovim-unwrapped.overrideAttrs (oldAttrs: {
         patches = oldAttrs.patches ++ [
--- hosts/nixos/packages.nix
+++ hosts/nixos/packages.nix
@@ -72,7 +72,6 @@ let
 
     notes = [
       dstask
-      logseq
       obsidian
       planify
       qownnotes
