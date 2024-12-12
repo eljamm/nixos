ad04fbc298d9e80b3361fd5e72ed919f0fc9b92e
eljamm <fedi.jamoussi@protonmail.ch>
2024-12-12 09:24:53 +0100

    feat(hosts/navi): use scx_flash scheduler

--- hardware/navi/default.nix
+++ hardware/navi/default.nix
@@ -30,9 +30,7 @@
     options nouveau modeset=0
   '';
 
-  # FIX:
-  # wait for: https://github.com/NixOS/nixpkgs/pull/363322
-  # # Fair scheduler with consistent performance
-  # # https://github.com/sched-ext/scx/blob/main/scheds/rust/scx_flash/README.md
-  # services.scx.scheduler = "scx_flash";
+  # Fair scheduler with consistent performance
+  # https://github.com/sched-ext/scx/blob/main/scheds/rust/scx_flash/README.md
+  services.scx.scheduler = "scx_flash";
 }
