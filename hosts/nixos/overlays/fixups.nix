1097f140a76624c102b5fb3317b1488a7f69e90e
eljamm <fedi.jamoussi@protonmail.ch>
2024-12-13 15:28:45 +0100

    bump(freetube): 0.22.0 -> 0.22.1

--- hosts/nixos/overlays/fixups.nix
+++ hosts/nixos/overlays/fixups.nix
@@ -1,5 +1,17 @@
 { pkgs, ... }:
 
 {
-  nixpkgs.overlays = [ ];
+  nixpkgs.overlays = [
+    (final: prev: {
+      freetube = prev.freetube.overrideAttrs rec {
+        pname = "freetube";
+        version = "0.22.1";
+        src = final.fetchurl {
+          url = "https://github.com/FreeTubeApp/FreeTube/releases/download/v${version}-beta/freetube_${version}_amd64.AppImage";
+          hash = "sha256-HbU5yRSP5Gk473ZoQL3HD2Bph/U/sq1Dd0eFpxyKc9s=";
+        };
+        appimageContents = pkgs.appimageTools.extractType2 { inherit pname version src; };
+      };
+    })
+  ];
 }
