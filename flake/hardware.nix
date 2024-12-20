75d81e4005311906fd4be5de121f410c9149bb9f
eljamm <fedi.jamoussi@protonmail.ch>
2024-12-20 09:31:51 +0100

    bump(linux-xanmod): 6.12.5 -> 6.12.6

--- flake/hardware.nix
+++ flake/hardware.nix
@@ -13,14 +13,14 @@
       boot.kernelPackages = pkgs.linuxPackagesFor (
         pkgs.linux_xanmod.override {
           argsOverride = rec {
-            version = "6.12.5";
+            version = "6.12.6";
             modDirVersion = "${version}-xanmod1";
 
             src = pkgs.fetchFromGitLab {
               owner = "xanmod";
               repo = "linux";
               rev = modDirVersion;
-              hash = "sha256-zawR9RMN/ATCiEbqlLOII0uvlHM6lZFlJ8fGjITYLPA=";
+              hash = "sha256-Pao4tS3SjllOkRrH+k/+9JqLIEOrNqJTFlHZ4cp2OF8=";
             };
           };
         }
