0afab7f6a5e463dae988488ca8ec58f6413d8bb1
eljamm <fedi.jamoussi@protonmail.ch>
2024-12-22 20:56:42 +0100

    feat: improve boot speed
    Shaves off about 1s on my system.

--- hosts/nixos/overlays/fixups.nix
+++ hosts/nixos/overlays/fixups.nix
@@ -1,6 +1,8 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
-  nixpkgs.overlays = [
-  ];
+  nixpkgs.overlays = [ ];
+
+  # https://discourse.nixos.org/t/get-to-the-login-screen-faster-on-nixos/57481
+  systemd.targets.network-online.wantedBy = lib.mkForce [ ];
 }
