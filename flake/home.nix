abd1fc8a2a5650984f00258f050f6ca221db0939
eljamm <fedi.jamoussi@protonmail.ch>
2024-12-22 21:11:37 +0100

    refactor: home-manager flake config
    We need to use `withSystem` so `inputs'` becomes available, after which
    we can call the package sets like `pkgsUnstable`.

--- flake/home.nix
+++ flake/home.nix
@@ -1,74 +1,79 @@
 {
-  inputs',
-  inputs,
   self,
+  inputs,
+  withSystem,
   ...
 }:
-let
-  args =
-    {
-      username ? "",
-    }:
-    {
-      inherit
-        self
-        inputs
-        inputs'
-        username
-        ;
-      pkgsCustom = inputs'.nixpkgs-custom.packages;
-      pkgsUnstable = inputs'.nixpkgs-unstable.legacyPackages;
-    };
-
-  commonModules = [
-    inputs.catppuccin.homeManagerModules.catppuccin
-    self.homeModules.git
-    self.homeModules.neovim
-    self.homeModules.shells
-    self.homeModules.style-catppuccin
-  ];
-
-  kuroModules = commonModules ++ [
-    self.homeModules.desktops-gnome
-    self.homeModules.desktops-hyprland
-    self.homeModules.terminals
-    ../hosts/nixos/users/kuroko/home
-  ];
-in
 {
-  flake.nixosModules.home-kuroko =
-    { pkgs, lib, ... }:
-    {
-      imports = [
-        inputs.home-manager.nixosModules.home-manager
+  flake = withSystem "x86_64-linux" (
+    ctx@{ inputs', ... }:
+    let
+      args =
+        {
+          username ? "",
+        }:
+        {
+          inherit
+            self
+            inputs
+            inputs'
+            username
+            ;
+          pkgsCustom = inputs'.nixpkgs-custom.packages;
+          pkgsUnstable = inputs'.nixpkgs-unstable.legacyPackages;
+        };
+
+      commonModules = [
+        inputs.catppuccin.homeManagerModules.catppuccin
+        self.homeModules.git
+        self.homeModules.neovim
+        self.homeModules.shells
+        self.homeModules.style-catppuccin
       ];
 
-      home-manager = {
-        useGlobalPkgs = true;
-        useUserPackages = true;
-        extraSpecialArgs = args { username = "kuroko"; };
-        users.kuroko.imports = kuroModules;
-      };
-    };
-
-  flake.nixosModules.home-navi =
-    { pkgs, lib, ... }:
+      kuroModules = commonModules ++ [
+        self.homeModules.desktops-gnome
+        self.homeModules.desktops-hyprland
+        self.homeModules.terminals
+        ../hosts/nixos/users/kuroko/home
+      ];
+    in
     {
-      imports = [ inputs.home-manager.nixosModules.home-manager ];
+      nixosModules.home-kuroko =
+        { pkgs, lib, ... }:
+        {
+          imports = [
+            inputs.home-manager.nixosModules.home-manager
+          ];
 
-      home-manager = {
-        useGlobalPkgs = true;
-        useUserPackages = true;
-        extraSpecialArgs = args { username = "navi"; };
-        users.navi.imports = commonModules ++ [
-          ../hosts/navi/home
-        ];
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            extraSpecialArgs = args { username = "kuroko"; };
+            users.kuroko.imports = kuroModules;
+          };
+        };
+
+      nixosModules.home-navi =
+        { pkgs, lib, ... }:
+        {
+          imports = [ inputs.home-manager.nixosModules.home-manager ];
+
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            extraSpecialArgs = args { username = "navi"; };
+            users.navi.imports = commonModules ++ [
+              ../hosts/navi/home
+            ];
+          };
+        };
+
+      homeConfigurations.kuroko = inputs.home-manager.lib.homeManagerConfiguration {
+        pkgs = inputs'.nixpkgs.legacyPackages;
+        modules = kuroModules;
+        extraSpecialArgs = args;
       };
-    };
-
-  flake.homeConfigurations.kuroko = inputs.home-manager.lib.homeManagerConfiguration {
-    pkgs = inputs'.nixpkgs.legacyPackages;
-    modules = kuroModules;
-    extraSpecialArgs = args;
-  };
+    }
+  );
 }
--- flake/hosts.nix
+++ flake/hosts.nix
@@ -1,6 +1,6 @@
 {
-  inputs,
   self,
+  inputs,
   withSystem,
   ...
 }:
