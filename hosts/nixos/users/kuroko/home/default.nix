7968a624c3c202886c69d135fd76492800a2ddfd
eljamm <fedi.jamoussi@protonmail.ch>
2025-01-03 09:56:19 +0100

    refactor: home-manager home config

--- hosts/nixos/users/kuroko/home/default.nix
+++ hosts/nixos/users/kuroko/home/default.nix
@@ -7,20 +7,6 @@
   ];
 
   home = {
-    # Home Manager needs a bit of information about you and the paths it should
-    # manage.
-    username = "kuroko";
-    homeDirectory = "/home/kuroko";
-
-    # This value determines the Home Manager release that your configuration is
-    # compatible with. This helps avoid breakage when a new Home Manager release
-    # introduces backwards incompatible changes.
-    #
-    # You should not change this value, even if you update Home Manager. If you do
-    # want to update the value, then make sure to first check the Home Manager
-    # release notes.
-    stateVersion = "23.11"; # Please read the comment before changing.
-
     packages = with pkgs; [
       difftastic
       moar
@@ -53,6 +39,15 @@
         MimeType=application/x-krita;
       '';
     };
+
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    stateVersion = "23.11"; # Please read the comment before changing.
   };
 
   services = {
