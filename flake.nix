9876f2f7ca4095c1169dc283314cca15504769d7
eljamm <fedi.jamoussi@protonmail.ch>
2025-01-06 18:14:21 +0100

    bump: update flake.lock

--- flake.nix
+++ flake.nix
@@ -66,7 +66,6 @@
     git-hooks-nix = {
       url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.nixpkgs-stable.follows = "nixpkgs";
     };
 
     treefmt-nix = {
