7113ab4d19ca73e1948831663fe91aa86e89d517
eljamm <fedi.jamoussi@protonmail.ch>
2025-01-07 21:49:51 +0100

    feat: allow unfree in pkgs

--- flake/infra.nix
+++ flake/infra.nix
@@ -12,6 +12,11 @@
       legacyPackages.lib = pkgs.callPackage ../lib.nix { inherit inputs; };
 
       _module.args = {
+        pkgs = import inputs.nixpkgs {
+          config.allowUnfree = true;
+          inherit system;
+        };
+
         # Flake argument for accessing the custom library more easily:
         # `perSystem = { devLib, ... }:`
         devLib = self.legacyPackages.${system}.lib;
