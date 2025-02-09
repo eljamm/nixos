b196ce49b73a76c8e415e7cfb70a70f3b79965a2
eljamm <fedi.jamoussi@protonmail.ch>
2025-02-09 11:51:10 +0100

    linux-xanmod: 6.12.11 -> 6.12.13

--- flake/hardware.nix
+++ flake/hardware.nix
@@ -13,14 +13,14 @@
       boot.kernelPackages = pkgs.linuxPackagesFor (
         pkgs.linux_xanmod.override {
           argsOverride = rec {
-            version = "6.12.11";
+            version = "6.12.13";
             modDirVersion = "${version}-xanmod1";
 
             src = pkgs.fetchFromGitLab {
               owner = "xanmod";
               repo = "linux";
               rev = modDirVersion;
-              hash = "sha256-miWefkd9m273TzPu1jqAp8Xch9LsiuoZQ6lZ2+2vxBA=";
+              hash = "sha256-1AlkY+7lkOnjvA+h3eP3RHPrV1upydDHhZVPY5ty58A=";
             };
           };
         }
