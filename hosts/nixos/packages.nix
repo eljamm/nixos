81d5969caaf2ca955a36ec643d36dbc168a5d665
eljamm <fedi.jamoussi@protonmail.ch>
2025-02-09 23:46:06 +0100

    feat: init super-productivity at 11.1.3

--- hosts/nixos/packages.nix
+++ hosts/nixos/packages.nix
@@ -79,6 +79,15 @@ let
       obsidian
       planify
       qownnotes
+      (super-productivity.overrideAttrs rec {
+        pname = "super-productivity";
+        version = "11.1.3";
+        src = fetchurl {
+          url = "https://github.com/johannesjo/super-productivity/releases/download/v${version}/superProductivity-x86_64.AppImage";
+          hash = "sha256-aRK/grVx9thwf/UG/SQpH6jfEL4dIO7XVeoFWEuVZ2Y=";
+          name = "${pname}-${version}.AppImage";
+        };
+      })
     ];
 
     qt5 = with libsForQt5; [
