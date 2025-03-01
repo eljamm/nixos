e7f44ca5263fb7e11e2bb497d1231c7e7b045af4
eljamm <fedi.jamoussi@protonmail.ch>
2025-03-01 05:40:52 +0100

    feat(service): enable syncthing

--- modules/nixos/services/misc.nix
+++ modules/nixos/services/misc.nix
@@ -1,4 +1,12 @@
-{ ... }:
+{ username, ... }:
 {
   services.xserver.wacom.enable = true;
+
+  services.syncthing = {
+    enable = true;
+    openDefaultPorts = true;
+    user = username;
+    group = "users";
+    dataDir = "/home/${username}/syncthing";
+  };
 }
