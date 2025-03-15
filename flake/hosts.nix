963d3c9e8ef12e1771eb6f8652ad7f537535268c
eljamm <fedi.jamoussi@protonmail.ch>
2025-03-15 10:15:52 +0100

    feat: disable unstable mesa
    Well, that was short lived :p

--- flake/hosts.nix
+++ flake/hosts.nix
@@ -17,7 +17,6 @@
             inputs.chaotic.nixosModules.default
             self.nixosModules.apps-main
             self.nixosModules.audio
-            self.nixosModules.chaoticMesa
             self.nixosModules.desktops
             self.nixosModules.dev-rust
             self.nixosModules.fonts
