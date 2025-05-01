77cf6f6af8a6e08ac560f5dbd13dabf95d226bfa
eljamm <fedi.jamoussi@protonmail.ch>
2025-05-01 08:52:55 +0200

    build(nixpkgs): nixos-24.11 -> nixos-unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-custom = {
       url = "github:eljamm/nixpkgs-custom";
@@ -16,7 +16,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
