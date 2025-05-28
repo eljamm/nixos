e3c0b725e210075f98a2ccb48e9b15a5db15c160
eljamm <fedi.jamoussi@protonmail.ch>
2025-05-28 08:49:52 +0200

    linux_xanmod: 6.14.7 -> 6.14.8

--- flake/hardware.nix
+++ flake/hardware.nix
@@ -6,14 +6,14 @@
       linux_xanmod_custom = pkgs.linuxPackagesFor (
         pkgs.linux_xanmod.override {
           argsOverride = rec {
-            version = "6.14.7";
+            version = "6.14.8";
             modDirVersion = "${version}-xanmod1";
 
             src = pkgs.fetchFromGitLab {
               owner = "xanmod";
               repo = "linux";
               rev = modDirVersion;
-              hash = "sha256-iyQirCCfjMq2hV4i+yPGX2ZFgvSmoxnWx7/hiIKlhr0=";
+              hash = "sha256-ap3NaM/pk+TWg3RBRxFJPwnfv2n042r47bo66jhj3ck=";
             };
           };
         }
