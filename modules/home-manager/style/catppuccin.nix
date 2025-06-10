684b2f3ff59bfdc136143e61a4a880788de52d01
eljamm <fedi.jamoussi@protonmail.ch>
2025-06-11 01:04:43 +0200

    yazi: temporarily disable catppuccin

--- modules/home-manager/style/catppuccin.nix
+++ modules/home-manager/style/catppuccin.nix
@@ -7,7 +7,7 @@
 
     bat.enable = true;
     kitty.enable = true;
-    yazi.enable = true;
+    yazi.enable = false; # FIX: https://github.com/catppuccin/nix/issues/577
 
     fzf = {
       enable = true;
