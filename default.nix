423176a9e5fe2818e5fd87a9c99234ac1b1a9753
eljamm <fedi.jamoussi@protonmail.ch>
2025-06-13 01:34:25 +0200

    feat: init default.nix

---
+++ default.nix
@@ -0,0 +1,28 @@
+let
+  flake-inputs = import (
+    fetchTarball "https://github.com/fricklerhandwerk/flake-inputs/tarball/4.1.0"
+  );
+  inherit (flake-inputs)
+    import-flake
+    ;
+in
+{
+  flake ? import-flake {
+    src = ./.;
+  },
+  sources ? flake.inputs,
+  system ? builtins.currentSystem,
+  pkgs ? import sources.nixpkgs {
+    config = { };
+    overlays = [ ];
+    inherit system;
+  },
+  lib ? import "${sources.nixpkgs}/lib",
+}:
+{
+  inherit
+    lib
+    pkgs
+    sources
+    ;
+}
