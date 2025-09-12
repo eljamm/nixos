6bc2bfe07ac6b2cd350fcdb1b467f72f17df92d3
eljamm <fedi.jamoussi@protonmail.ch>
2025-09-12 17:04:14 +0200

    redlib: unstable -> 0.36.0-unstable-2025-09-09

--- hosts/nixos/services.nix
+++ hosts/nixos/services.nix
@@ -16,7 +16,20 @@
 
   # Reddit
   services.redlib.enable = true;
-  services.redlib.package = pkgsUnstable.redlib;
+  services.redlib.package = pkgsUnstable.redlib.overrideAttrs rec {
+    version = "0.36.0-unstable-2025-09-09";
+    src = pkgsUnstable.fetchFromGitHub {
+      owner = "redlib-org";
+      repo = "redlib";
+      rev = "a989d19ca92713878e9a20dead4252f266dc4936";
+      hash = "sha256-YJZVkCi8JQ1U47s52iOSyyf32S3b35pEqw4YTW8FHVY=";
+    };
+
+    cargoDeps = pkgsUnstable.rustPlatform.fetchCargoVendor {
+      inherit src;
+      hash = "sha256-L35VSQdIbKGGsBPU2Sj/MoYohy1ZibgZ+7NVa3yNjH8=";
+    };
+  };
 
   services.vikunja.enable = true;
   services.vikunja.frontendScheme = "http";
