2b779ea749eaabad33fd1b92b60eaaccb998d9c5
eljamm <fedi.jamoussi@protonmail.ch>
2025-09-15 00:41:19 +0200

    freetube: 0.23.8 -> 0.23.9

--- hosts/nixos/overlays/pinned.nix
+++ hosts/nixos/overlays/pinned.nix
@@ -32,16 +32,16 @@
       freetube = prev.freetube.overrideAttrs (
         finalAttrs: _: {
           pname = "freetube";
-          version = "0.23.8";
+          version = "0.23.9";
           src = final.fetchFromGitHub {
             owner = "FreeTubeApp";
             repo = "FreeTube";
             tag = "v${finalAttrs.version}-beta";
-            hash = "sha256-CHp/6/E/v6UdSe3xoB66Ot24WuZDPdmNyUG1w2w3bX0=";
+            hash = "sha256-KTVGmYlDOqugTbC4YQcoTUW+Ja9y1H6mwULrMkZVI0Y=";
           };
           yarnOfflineCache = final.fetchYarnDeps {
             yarnLock = "${finalAttrs.src}/yarn.lock";
-            hash = "sha256-ia5wLRt3Hmo4/dsB1/rhGWGJ7LMnVR9ju9lSlQZDTTg=";
+            hash = "sha256-CnOxWA36D9x+p/Xx6sE865MSyTgPt054SX+ASr9K7zI=";
           };
         }
       );
