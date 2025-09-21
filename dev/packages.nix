dd947fdaa4deaa216bdf19e80643f75f8b1ea799
eljamm <fedi.jamoussi@protonmail.ch>
2025-09-21 17:58:24 +0200

    linux_xanmod: 6.16.7 -> 6.16.8

--- dev/packages.nix
+++ dev/packages.nix
@@ -6,14 +6,14 @@
 {
   linux_xanmod_custom = pkgs.linux_xanmod_latest.override {
     argsOverride = rec {
-      version = "6.16.7";
+      version = "6.16.8";
       modDirVersion = "${version}-xanmod1";
 
       src = pkgs.fetchFromGitLab {
         owner = "xanmod";
         repo = "linux";
         rev = modDirVersion;
-        hash = "sha256-/CFSGaDbK0pZgGGOOxixwOQgeD1OsbUhtRss4VbXHxE=";
+        hash = "sha256-8CMzqXKbd605TU4XhPqUHVECrmW0OYbazZ4RpSji974=";
       };
     };
   };
