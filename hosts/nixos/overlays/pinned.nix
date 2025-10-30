d29a0e1538800830ffce6335aadd657851da4fc7
eljamm <fedi.jamoussi@protonmail.ch>
2025-10-30 07:12:00 +0100

    yt-dlp: 2025.10.14 -> 2025.10.22

--- hosts/nixos/overlays/pinned.nix
+++ hosts/nixos/overlays/pinned.nix
@@ -71,12 +71,12 @@
     })
     (final: prev: {
       yt-dlp = prev.yt-dlp.overridePythonAttrs (oldAttrs: rec {
-        version = "2025.10.14";
+        version = "2025.10.22";
         src = final.fetchFromGitHub {
           owner = "yt-dlp";
           repo = "yt-dlp";
           tag = version;
-          hash = "sha256-x7vpuXUihlC4jONwjmWnPECFZ7xiVAOFSDUgBNvl+aA=";
+          hash = "sha256-jQaENEflaF9HzY/EiMXIHgUehAJ3nnDT9IbaN6bDcac=";
         };
       });
     })
