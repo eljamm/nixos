66e2a42ba93133707b91f4430e0658b7a182a73e
eljamm <fedi.jamoussi@protonmail.ch>
2025-11-09 21:27:13 +0100

    fix: yazi home package

--- modules/home-manager/programs/yazi/default.nix
+++ modules/home-manager/programs/yazi/default.nix
@@ -5,31 +5,33 @@
   ...
 }:
 {
+  # TODO: investigate why the module options don't work ...
+  # files are linked, but the config isn't applied
   programs.yazi = {
     enable = true;
     enableFishIntegration = true;
-    package = pkgsUnstable.yazi;
-    initLua = ./init.lua;
-    settings.keymap = lib.importTOML ./keymap.toml;
-    settings.yazi = lib.importTOML ./yazi.toml;
-    plugins = {
-      inherit (pkgsUnstable.yaziPlugins)
-        full-border
-        git
-        jump-to-char
+    package = pkgsUnstable.yazi.override {
+      initLua = ./init.lua;
+      settings.keymap = lib.importTOML ./keymap.toml;
+      settings.yazi = lib.importTOML ./yazi.toml;
+      plugins = {
+        inherit (pkgsUnstable.yaziPlugins)
+          full-border
+          git
+          jump-to-char
+          mediainfo
+          smart-enter
+          smart-filter
+          smart-paste
+          starship
+          toggle-pane
+          ;
+        bookmarks = pkgsUnstable.callPackage ./bookmarks.nix { };
+        compress = pkgsUnstable.callPackage ./compress.nix { };
+      };
+      extraPackages = with pkgs; [
         mediainfo
-        smart-enter
-        smart-filter
-        smart-paste
-        starship
-        toggle-pane
-        ;
-      bookmarks = pkgsUnstable.callPackage ./bookmarks.nix { };
-      compress = pkgsUnstable.callPackage ./compress.nix { };
+      ];
     };
-    # TODO: 25.11
-    # extraPackages = with pkgs; [
-    #   mediainfo
-    # ];
   };
 }
