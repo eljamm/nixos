3e9b512c43a9555b04b529227fb24eadc53986dd
eljamm <fedi.jamoussi@protonmail.ch>
2025-11-09 22:49:17 +0100

    yazi: fix module

--- modules/home-manager/programs/yazi/default.nix
+++ modules/home-manager/programs/yazi/default.nix
@@ -5,35 +5,33 @@
   ...
 }:
 {
-  # TODO: investigate why the module options don't work ...
-  # files are linked, but the config isn't applied
   programs.yazi = {
     enable = true;
     enableFishIntegration = true;
     package = pkgsUnstable.yazi.override {
-      initLua = ./init.lua;
-      settings.keymap = lib.importTOML ./keymap.toml;
-      settings.yazi = lib.importTOML ./yazi.toml;
-      plugins = {
-        inherit (pkgsUnstable.yaziPlugins)
-          full-border
-          git
-          jump-to-char
-          mediainfo
-          recycle-bin
-          smart-enter
-          smart-filter
-          smart-paste
-          starship
-          toggle-pane
-          ;
-        bookmarks = pkgsUnstable.callPackage ./bookmarks.nix { };
-        compress = pkgsUnstable.callPackage ./compress.nix { };
-      };
       extraPackages = with pkgs; [
         mediainfo
         trash-cli
       ];
     };
+    plugins = {
+      inherit (pkgsUnstable.yaziPlugins)
+        full-border
+        git
+        jump-to-char
+        mediainfo
+        recycle-bin
+        smart-enter
+        smart-filter
+        smart-paste
+        starship
+        toggle-pane
+        ;
+      bookmarks = pkgsUnstable.callPackage ./bookmarks.nix { };
+      compress = pkgsUnstable.callPackage ./compress.nix { };
+    };
+    initLua = ./init.lua;
+    keymap = lib.importTOML ./keymap.toml;
+    settings = lib.importTOML ./yazi.toml;
   };
 }
