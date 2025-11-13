f1e9a6e2be5df7986db3a4a120bcae956630f8f9
eljamm <fedi.jamoussi@protonmail.ch>
2025-11-13 08:16:32 +0100

    refactor: default.nix

--- default.nix
+++ default.nix
@@ -12,65 +12,63 @@
   lib ? import "${inputs.nixpkgs}/lib",
 }:
 let
-  scope = lib.makeScope pkgs.newScope (
-    self': with self'; {
-      inherit
-        lib
-        pkgs
-        self
-        system
-        inputs
+  scope = lib.makeScope pkgs.newScope (sc: {
+    inherit
+      lib
+      pkgs
+      self
+      system
+      inputs
+      ;
+
+    # Custom library. Contains helper functions, builders, ...
+    devLib = sc.callPackage ./dev/utils.nix { };
+
+    pkgsCustom = inputs.nixpkgs-custom.legacyPackages.${system} // {
+      agenix = inputs.agenix.packages.${system}.default;
+    };
+
+    pkgsUnstable = import inputs.nixpkgs-unstable {
+      config.allowUnfree = true;
+      inherit system;
+    };
+
+    hosts = sc.callPackage ./hosts { };
+
+    modules = sc.devLib.mkModules ./modules;
+    hardwareModules = sc.devLib.mkModules ./modules/hardware;
+    nixosModules = sc.devLib.mkModules ./modules/nixos;
+    homeModules = sc.devLib.mkModules ./modules/home-manager;
+
+    format = sc.callPackage ./dev/formatter.nix { };
+    devPkgs = lib.filterAttrs (n: v: lib.isDerivation v) (sc.callPackage ./dev/packages.nix { });
+    devShells.default = pkgs.mkShellNoCC {
+      packages = [
+        sc.format.formatter
+      ];
+    };
+
+    overlays.default = final: prev: sc.devPkgs;
+
+    flake.perSystem = {
+      devShells = sc.devShells;
+      formatter = sc.format.formatter;
+      packages = sc.devPkgs;
+      checks = lib.filterAttrs (_: v: !v.meta.broken or false) sc.flake.perSystem.packages;
+      legacyPackages = {
+        lib = sc.devLib;
+        packages = sc.devPkgs;
+      };
+    };
+    flake.system-agnostic = {
+      inherit (sc)
+        overlays
+        homeModules
+        nixosModules
+        hardwareModules
         ;
-
-      # Custom library. Contains helper functions, builders, ...
-      devLib = callPackage ./dev/utils.nix { };
-
-      pkgsCustom = inputs.nixpkgs-custom.legacyPackages.${system} // {
-        agenix = inputs.agenix.packages.${system}.default;
-      };
-
-      pkgsUnstable = import inputs.nixpkgs-unstable {
-        config.allowUnfree = true;
-        inherit system;
-      };
-
-      hosts = callPackage ./hosts { };
-
-      modules = devLib.mkModules ./modules;
-      hardwareModules = devLib.mkModules ./modules/hardware;
-      nixosModules = devLib.mkModules ./modules/nixos;
-      homeModules = devLib.mkModules ./modules/home-manager;
-
-      format = callPackage ./dev/formatter.nix { };
-      devPkgs = lib.filterAttrs (n: v: lib.isDerivation v) (callPackage ./dev/packages.nix { });
-      devShells.default = pkgs.mkShellNoCC {
-        packages = [
-          format.formatter
-        ];
-      };
-
-      overlays.default = final: prev: devPkgs;
-
-      flake.system-agnostic = {
-        inherit
-          overlays
-          homeModules
-          nixosModules
-          hardwareModules
-          ;
-        nixosConfigurations = hosts;
-      };
-      flake.perSystem = {
-        devShells = devShells;
-        formatter = format.formatter;
-        packages = devPkgs;
-        checks = lib.filterAttrs (_: v: !v.meta.broken or false) flake.perSystem.packages;
-        legacyPackages = {
-          lib = devLib;
-          packages = devPkgs;
-        };
-      };
-    }
-  );
+      nixosConfigurations = sc.hosts;
+    };
+  });
 in
 scope // scope.devPkgs // scope.hosts
