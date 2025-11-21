dd2ceab17d4c4766238c47abc86fe745ba6ee786
eljamm <fedi.jamoussi@protonmail.ch>
2025-11-21 22:53:05 +0100

    docs: add a few notes to simpleScope

--- dev/lib/simple-scope.nix
+++ dev/lib/simple-scope.nix
@@ -12,13 +12,20 @@ let
         newScope = scope: newScope (self // scope);
         overrideScope = g: simpleScope newScope (lib.extends g f);
         callPackage = self.newScope { };
+
+        # Compute a scope's fixpoint using `callPackage`
+        # Example: finalScope = scope.fix scope;
+        # See: https://nixos.org/manual/nixpkgs/unstable/#sec-functions-library-fixedPoints
+        fix = f;
+
+        # Automatically get arguments from `callPackage`, but don't return an
+        # overridable result. Useful for importing files in the top-level.
         call =
           f: args:
           removeAttrs (self.callPackage f args) [
             "override"
             "overrideDerivation"
           ];
-        fix = f;
       };
     in
     self;
