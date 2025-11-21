42c37267f46f2ad9d5893784bfa054dfa6a89465
eljamm <fedi.jamoussi@protonmail.ch>
2025-11-21 20:46:51 +0100

    refactor: default.nix

--- default.nix
+++ default.nix
@@ -3,26 +3,25 @@
   inputs ? self.inputs,
   system ? builtins.currentSystem,
   pkgs ? import inputs.nixpkgs {
-    config = {
-      allowBroken = true;
-    };
+    config.allowBroken = true;
     overlays = [ ];
     inherit system;
   },
   lib ? import "${inputs.nixpkgs}/lib",
 }:
 let
-  scope = lib.makeScope pkgs.newScope (sc: {
+  scope = lib.makeScope pkgs.newScope (s: {
     inherit
       lib
       pkgs
       self
       system
       inputs
+      flake
       ;
 
     # Custom library. Contains helper functions, builders, ...
-    devLib = sc.callPackage ./dev/utils.nix { };
+    devLib = s.callPackage ./dev/utils.nix { };
 
     pkgsCustom = inputs.nixpkgs-custom.legacyPackages.${system} // {
       agenix = inputs.agenix.packages.${system}.default;
@@ -33,39 +32,47 @@ let
       inherit system;
     };
 
-    formatter = sc.callPackage ./dev/formatter.nix { };
-    devPkgs = lib.filterAttrs (n: v: lib.isDerivation v) (sc.callPackage ./dev/packages.nix { });
+    formatter = s.callPackage ./dev/formatter.nix { };
+    packages = s.callPackage ./dev/packages.nix { };
     devShells.default = pkgs.mkShellNoCC {
       packages = [
-        sc.formatter.package
+        s.formatter.package
       ];
     };
 
-    hosts = sc.callPackage ./hosts { };
-    modules = sc.devLib.mkModules ./modules;
+    hosts = s.callPackage ./hosts { };
+    modules = s.devLib.mkModules ./modules;
 
-    overlays.default = final: prev: sc.devPkgs;
+    overlays.default = final: prev: s.devPkgs;
+  });
 
-    flake.perSystem = {
-      devShells = sc.devShells;
-      formatter = sc.formatter.package;
-      packages = sc.devPkgs;
-      checks = lib.filterAttrs (_: v: !v.meta.broken or false) sc.flake.perSystem.packages;
+  # final attribute set (non-recursive)
+  finalScope = scope.packages scope;
+
+  flake = with finalScope; {
+    # depends on the system (e.g. packages.x86_64-linux)
+    perSystem = {
+      devShells = devShells;
+      formatter = formatter.package;
+      packages = lib.filterAttrs (n: v: lib.isDerivation v) packages;
+      checks = lib.filterAttrs (_: v: !v.meta.broken or false) flake.perSystem.packages;
       legacyPackages = {
-        lib = sc.devLib;
-        packages = sc.devPkgs;
+        lib = devLib;
+        packages = flake.perSystem.packages;
       };
     };
-    flake.systemAgnostic = {
-      inherit (sc) overlays;
 
-      formatterModule = sc.formatter.module;
-      hardwareModules = sc.modules.hardware;
-      homeModules = sc.modules.home-manager;
-      nixosModules = sc.modules.nixos;
+    # system-independant (e.g. nixosModules)
+    systemAgnostic = {
+      overlays = overlays;
 
-      nixosConfigurations = sc.hosts;
+      formatterModule = formatter.module;
+      hardwareModules = modules.hardware;
+      homeModules = modules.home-manager;
+      nixosModules = modules.nixos;
+
+      nixosConfigurations = hosts;
     };
-  });
+  };
 in
-scope
+finalScope
