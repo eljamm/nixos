52d95c38b25e89c120131bc3c48577f705f8031d
eljamm <fedi.jamoussi@protonmail.ch>
2025-12-07 17:59:57 +0100

    redlib: 0.36.0-unstable-2025-09-09 -> 0.36.0-unstable-2025-10-06

--- hosts/nixos/services.nix
+++ hosts/nixos/services.nix
@@ -16,20 +16,25 @@
 
   # Reddit
   services.redlib.enable = true;
-  services.redlib.package = pkgsUnstable.redlib.overrideAttrs rec {
-    version = "0.36.0-unstable-2025-09-09";
+  services.redlib.package = pkgsUnstable.redlib.overrideAttrs (oldAttrs: rec {
+    version = "0.36.0-unstable-2025-10-06";
     src = pkgsUnstable.fetchFromGitHub {
       owner = "redlib-org";
       repo = "redlib";
-      rev = "a989d19ca92713878e9a20dead4252f266dc4936";
-      hash = "sha256-YJZVkCi8JQ1U47s52iOSyyf32S3b35pEqw4YTW8FHVY=";
+      rev = "2dc6b5f3c0db1f8e78a74048ba4550ba6202cb55";
+      hash = "sha256-Di3ZZZ4UqR00ud6MdrnJGUngdd/RSC1uNKlsmTdUx2k=";
     };
 
     cargoDeps = pkgsUnstable.rustPlatform.fetchCargoVendor {
       inherit src;
-      hash = "sha256-L35VSQdIbKGGsBPU2Sj/MoYohy1ZibgZ+7NVa3yNjH8=";
+      hash = "sha256-VLXRnSICpMOj/4ebhNSwWH9cwAGTz44kQW6Fa02nwIs=";
     };
-  };
+
+    checkFlags = oldAttrs.checkFlags ++ [
+      "--skip=test_generic_web_backend"
+      "--skip=test_mobile_spoof_backend"
+    ];
+  });
 
   services.vikunja.enable = true;
   services.vikunja.frontendScheme = "http";
