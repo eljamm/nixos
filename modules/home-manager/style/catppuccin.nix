2919ebbc8246ce1760b6a6212b938006130fc9d2
eljamm <fedi.jamoussi@protonmail.ch>
2026-01-06 17:43:51 +0100

    style(catppuccin): support newer yazi version

--- modules/home-manager/style/catppuccin.nix
+++ modules/home-manager/style/catppuccin.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   catppuccin = {
     enable = true;
@@ -20,5 +20,13 @@
     nvim.enable = false;
     starship.enable = false;
     zellij.enable = false;
+
+    # support v25.12.29
+    sources.yazi = pkgs.fetchFromGitHub {
+      owner = "catppuccin";
+      repo = "yazi";
+      rev = "fc69d6472d29b823c4980d23186c9c120a0ad32c";
+      hash = "sha256-Og33IGS9pTim6LEH33CO102wpGnPomiperFbqfgrJjw=";
+    };
   };
 }
