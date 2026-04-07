39e7d7e716636c85fafdd6b8c24f706ec2a873f7
eljamm <fedi.jamoussi@protonmail.ch>
2026-04-07 23:26:11 +0200

    readest: 0.9.99 -> 0.10.4

--- hosts/nixos/overlays/pinned.nix
+++ hosts/nixos/overlays/pinned.nix
@@ -53,23 +53,69 @@
       readest = devLib.newestPackage pkgsUnstable (
         pkgsUnstable.readest.overrideAttrs (
           finalAttrs: oldAttrs: {
-            version = "0.9.99";
+            version = "0.10.4";
+
             src = final.fetchFromGitHub {
               owner = "readest";
               repo = "readest";
               tag = "v${finalAttrs.version}";
-              hash = "sha256-Fcil35siaGrooW8+R2WrZaR5qHPJXIYOU/Au1YKlb2M=";
+              hash = "sha256-/4UyRzFALujZ98EdXxiVeLH8W+0Mqm09RZ4zEwOVyyk=";
               fetchSubmodules = true;
             };
-            pnpmDeps = final.pnpm_10.fetchDeps {
+
+            pnpmDeps = final.fetchPnpmDeps {
               inherit (finalAttrs) pname version src;
               pnpm = final.pnpm_10;
               fetcherVersion = 3;
-              hash = "sha256-/bzjOdpvuPLBMvX/q1WaO3lFg5/jLz5Ypr5OojssXUI=";
+              hash = "sha256-7jar0aNXaBfoklGzgvC+1DwsOXgpnOcIcEbIGwWgyfw=";
             };
+
             cargoDeps = final.rustPlatform.fetchCargoVendor {
               inherit (finalAttrs) src;
-              hash = "sha256-qYBHYjwfGkKmGXN8caamZ6/XGtnxe+lmy6dIpdMwS/I=";
+              hash = "sha256-rxOjXhXN19w8qAEvELIh0oXuB/N80Dtzmf/i3hjRal0=";
+            };
+
+            postPatch = oldAttrs.postPatch + ''
+              mkdir -p src-tauri/plugins/tauri-plugin-turso/dist-js
+              cp -r ${finalAttrs.passthru.tursoPlugin} src-tauri/plugins/tauri-plugin-turso/dist-js
+              jq '.scripts.build = "true"' \
+                src-tauri/plugins/tauri-plugin-turso/package.json | \
+                sponge src-tauri/plugins/tauri-plugin-turso/package.json
+            '';
+
+            preBuild = oldAttrs.preBuild + ''
+              pnpm --filter @readest/readest-app build
+            '';
+
+            passthru.tursoPluginDeps = final.fetchPnpmDeps {
+              pname = "tauri-plugin-turso";
+              version = finalAttrs.version;
+              src = "${finalAttrs.src}/apps/readest-app/src-tauri/plugins/tauri-plugin-turso";
+              pnpm = final.pnpm_10;
+              fetcherVersion = 3;
+              hash = "sha256-Jf/UaEaLUg/v9ZRInBCEfkDY4d6nwyAIegCMKZe0iAQ=";
+            };
+
+            passthru.tursoPlugin = final.stdenv.mkDerivation {
+              pname = "tauri-plugin-turso";
+              version = finalAttrs.version;
+              src = "${finalAttrs.src}/apps/readest-app/src-tauri/plugins/tauri-plugin-turso";
+
+              nativeBuildInputs = with final; [
+                pnpm_10
+                pnpmConfigHook
+                nodejs
+              ];
+
+              pnpmDeps = finalAttrs.passthru.tursoPluginDeps;
+
+              buildPhase = ''
+                pnpm build
+              '';
+
+              installPhase = ''
+                cp -r dist-js $out
+              '';
             };
           }
         )
