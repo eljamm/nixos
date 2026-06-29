fa25b92f81aad835e3cf14b740491e35c04bb046
eljamm <fedi.jamoussi@protonmail.ch>
2026-06-29 13:45:19 +0200

    feat: add firefly-iii backup service

--- modules/nixos/services/firefly-iii.nix
+++ modules/nixos/services/firefly-iii.nix
@@ -1,5 +1,6 @@
 {
   config,
+  pkgs,
   lib,
   ...
 }:
@@ -61,5 +62,62 @@ in
       cfg.virtualHost
       cfgImporter.virtualHost
     ];
+  };
+
+  systemd.services.firefly-iii-backup = {
+    description = "Firefly III backup";
+
+    serviceConfig = {
+      Type = "oneshot";
+    };
+
+    unitConfig =
+      let
+        services = [
+          "phpfpm-firefly-iii.service"
+        ]
+        ++ lib.optional cfgImporter.enable "phpfpm-firefly-iii-data-importer.service";
+      in
+      {
+        # Shut down the firefly-iii services while the backup runs
+        Conflicts = services;
+        After = services;
+        # Bring them back up afterwards, regardless of pass/fail
+        OnSuccess = services;
+        OnFailure = services;
+      };
+
+    script = ''
+      set -euo pipefail
+
+      backupDir="${cfg.dataDir}/backup"
+      timestamp=$(date +%Y%m%d-%H%M%S)
+
+      mkdir -p "$backupDir"
+
+      ${lib.getBin pkgs.sqlite}/bin/sqlite3 \
+        "${cfg.dataDir}/storage/database/database.sqlite" \
+        ".backup '$backupDir/firefly-iii-database-$timestamp.sqlite'"
+
+      ${lib.optionalString cfgImporter.enable ''
+        ${lib.getBin pkgs.gnutar}/bin/tar \
+          -c \
+          -I ${lib.getBin pkgs.gzip}/bin/gzip \
+          -f "$backupDir/firefly-iii-data-importer-$timestamp.tar.gz" \
+          -C "${cfgImporter.dataDir}/storage" \
+          configurations import-jobs
+      ''}
+
+      ${lib.getBin pkgs.fd}/bin/fd \
+        -g "firefly-iii-*" \
+        "$backupDir" \
+        --changed-before '30d' \
+        -X rm
+
+      ${lib.getBin pkgs.coreutils}/bin/chown -R "${cfg.user}:${cfg.group}" "$backupDir"
+      ${lib.getBin pkgs.coreutils}/bin/chmod 0700 "$backupDir"
+    '';
+
+    startAt = "daily";
   };
 }
