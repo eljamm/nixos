b340884894d7435b3925f4e4c57055a18a9fa1bc
eljamm <fedi.jamoussi@protonmail.ch>
2026-07-08 16:14:35 +0200

    Revert "linux_xanmod: 7.0.12 -> 7.1.3"
    This reverts commit 6a9dd686d5243240b7021691ca3519cf316a305c.

--- hosts/nixos/overlays/pinned.nix
+++ hosts/nixos/overlays/pinned.nix
@@ -117,8 +117,8 @@
     (final: prev: {
       linux_xanmod_custom =
         let
-          version = "7.1.3";
-          hash = "sha256-EvxKUWDp0KAKhYO4rZKKx22RCGOimzPm5epchME/pJ8=";
+          version = "7.0.12";
+          hash = "sha256-yqyHkdSLSyTrhnwsWJ0jq4bhqaCrJ+qzkUzxdrVJP6A=";
           modDirVersion = "${version}-xanmod1";
           kernel = prev.linux_xanmod_latest;
         in
