fb1801f18f1c228c41e1781b64ccddb4d0b094c1
eljamm <fedi.jamoussi@protonmail.ch>
2026-09-09 13:00:46 +0200

    build: update flake.lock

--- flake.nix
+++ flake.nix
@@ -50,7 +50,7 @@
 
     spicetify-nix = {
       url = "github:Gerg-L/spicetify-nix";
-      # inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
     blender-bin = {
