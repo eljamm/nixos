40c4ba46df61188ebfcff55d806ab9c52e792df3
eljamm <fedi.jamoussi@protonmail.ch>
2026-09-13 06:40:36 +0200

    linux_xanmod: 7.2.4 -> 7.2.5

--- hosts/nixos/overlays/pinned.nix
+++ hosts/nixos/overlays/pinned.nix
@@ -132,8 +132,8 @@
     (final: prev: {
       linux_xanmod_custom =
         let
-          version = "7.2.4";
-          hash = "sha256-Qih/0v0WE5XJueOn93JhG0WhEaI1bfFi0wua4yP9zSo=";
+          version = "7.2.5";
+          hash = "sha256-PQDZPcIc+F7dAkb0/3S0Ft7b2JYvQ9BldSyCifEIcpM=";
           modDirVersion = "${version}-xanmod1";
           kernel = prev.linux_xanmod_latest;
         in
